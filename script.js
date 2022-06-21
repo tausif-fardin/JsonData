801ae8062a23b4c60ad51965c5d90833037e03f1
Tausif Fardin <tausiffardin4@gmail.com>
2022-06-21 13:39:50 +0600

    Added modals

--- script.js
+++ script.js
@@ -59,6 +59,9 @@ function onFormSubmit() {
     else
         updateRecord(formData);
     resetForm();
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+
 }
 
 function readFormData() {
@@ -85,12 +88,8 @@ function insertNewRecord(data) {
     cell4 = newRow.insertCell(4);
     cell4.innerHTML = data.gender;
     cell5 = newRow.insertCell(5);
-    cell5.innerHTML = `<button class="btn btn-primary m-3"  onClick="onEdit(this)">Edit</button>
+    cell5.innerHTML = `<button class="btn btn-primary m-3" onClick="onEdit(this); openModal();">Edit</button>
     <button class="btn btn-primary m-3" onClick="onDelete(this)">Delete</button>`;
-
-
-
-
 }
 
 function resetForm() {
@@ -103,7 +102,6 @@ function resetForm() {
 }
 
 function onEdit(td) {
-
     selectedRow = td.parentElement.parentElement;
     document.getElementById("id").value = selectedRow.cells[0].innerHTML;
     document.getElementById("firstname").value = selectedRow.cells[1].innerHTML;
@@ -124,5 +122,32 @@ function onDelete(td) {
         row = td.parentElement.parentElement;
         document.getElementById("customer_table").deleteRow(row.rowIndex);
         resetForm();
+    }
+}
+openModal();
+function openModal() {
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("btnAdd");
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal 
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
     }
 }
