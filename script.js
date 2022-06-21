61f90ab0b043188702392aa0f051386d313f08d2
Tausif Fardin <tausiffardin4@gmail.com>
2022-06-21 12:10:12 +0600

    Added crud feature

---
+++ script.js
@@ -0,0 +1,128 @@
+
+let data = [
+    {
+        id: 1,
+        first_name: "Juanita",
+        last_name: "Churchlow",
+        email: "jchurchlow0@mysql.com",
+        gender: "Female"
+    },
+    {
+        id: 2,
+        first_name: "Odie",
+        last_name: "Gheorghie",
+        email: "ogheorghie1@bing.com",
+        gender: "Male"
+    },
+    {
+        id: 3,
+        first_name: "Irwin",
+        last_name: "Guye",
+        email: "iguye2@mac.com",
+        gender: "Male"
+    },
+    {
+        id: 4,
+        first_name: "Dacy",
+        last_name: "Facher",
+        email: "dfacher3@ucsd.edu",
+        gender: "Female"
+    },
+    {
+        id: 5,
+        first_name: "Brenn",
+        last_name: "Hancill",
+        email: "bhancill4@slideshare.net",
+        gender: "Female"
+    }
+]
+let table = document.getElementById('customer_table');
+data.map((data) => {
+    let row = `<tr>
+            <td class="col-2 col-xs-2 col-md-2">${data.id}</td>
+            <td  class="col-2 col-xs-2 col-md-2">${data.first_name}</td>
+            <td  class="col-2 col-xs-2 col-md-2">${data.last_name}</td>
+            <td  class="col-2 col-xs-2 col-md-2">${data.email}</td>
+            <td  class="col-2 col-xs-2 col-md-2">${data.gender}</td>
+            <td  class="col-2 col-xs-2 col-md-2"><button class="btn btn-primary m-3" onClick="onEdit(this)">Edit</button>
+            <button class="btn btn-primary m-3" onClick="onDelete(this)">Delete</button></td>          
+            </tr>`
+    table.innerHTML += row;
+});
+
+var selectedRow = null
+
+function onFormSubmit() {
+    var formData = readFormData();
+    if (selectedRow == null)
+        insertNewRecord(formData);
+    else
+        updateRecord(formData);
+    resetForm();
+}
+
+function readFormData() {
+    var formData = {};
+    formData["id"] = document.getElementById("id").value;
+    formData["firstname"] = document.getElementById("firstname").value;
+    formData["lastname"] = document.getElementById("lastname").value;
+    formData["email"] = document.getElementById("email").value;
+    formData["gender"] = document.getElementById("gender").value;
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById("customer_table").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.id;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.firstname;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.lastname;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.email;
+    cell4 = newRow.insertCell(4);
+    cell4.innerHTML = data.gender;
+    cell5 = newRow.insertCell(5);
+    cell5.innerHTML = `<button class="btn btn-primary m-3"  onClick="onEdit(this)">Edit</button>
+    <button class="btn btn-primary m-3" onClick="onDelete(this)">Delete</button>`;
+
+
+
+
+}
+
+function resetForm() {
+    document.getElementById("id").value = "";
+    document.getElementById("firstname").value = "";
+    document.getElementById("lastname").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("gender").value = "";
+    selectedRow = null;
+}
+
+function onEdit(td) {
+
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("id").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("firstname").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("lastname").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("email").value = selectedRow.cells[3].innerHTML;
+    document.getElementById("gender").value = selectedRow.cells[4].innerHTML;
+}
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.id;
+    selectedRow.cells[1].innerHTML = formData.firstname;
+    selectedRow.cells[2].innerHTML = formData.lastname;
+    selectedRow.cells[3].innerHTML = formData.email;
+    selectedRow.cells[4].innerHTML = formData.gender;
+}
+
+function onDelete(td) {
+    if (confirm('Are you sure to delete this record ?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById("customer_table").deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
